c9741e0ea1f730b7341d6df445572db76e41e376
chrisribia <chrisribia@gmail.com>
2019-09-24 11:52:24 +0300

    implement employee repository

--- app/src/main/java/com/example/bargraph/data/Repository/EmployeeRepository.kt
+++ app/src/main/java/com/example/bargraph/data/Repository/EmployeeRepository.kt
@@ -1,4 +1,16 @@
 package com.example.bargraph.data.Repository
 
-class EmployeeRepository {
+import com.example.bargraph.data.db.AppDataBase
+import com.example.bargraph.data.db.Entity.Employee
+import com.example.bargraph.data.network.MyApi
+import com.example.bargraph.data.network.SafeApiRequest
+
+class EmployeeRepository(
+    private val api : MyApi,
+    private val db : AppDataBase
+) : SafeApiRequest(){
+
+    suspend fun saveEmployees(employees: List<Employee>) = db.getEmployeeDao().saveAllEmployees(employees)
+
+    fun getEmployees() = db.getEmployeeDao().getEmployees()
 }
