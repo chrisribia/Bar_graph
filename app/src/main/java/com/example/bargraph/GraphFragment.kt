89870345b251d367a06cf0942fb264a3e71103ca
chrisribia <chrisribia@gmail.com>
2019-09-24 10:43:05 +0300

    add more dependancies

--- app/src/main/java/com/example/bargraph/GraphFragment.kt
+++ app/src/main/java/com/example/bargraph/GraphFragment.kt
@@ -6,19 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.github.mikephil.charting.data.BarEntry
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class GraphFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_graph, container, false)
+       val mview : View = inflater.inflate(R.layout.fragment_graph, container, false)
+
+    return mview
     }
+
+
 
 
 }
