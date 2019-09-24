3b7b4fdb39dc0a2c72746e537e511b1479fbde95
chrisribia <chrisribia@gmail.com>
2019-09-24 10:58:28 +0300

    Add Employee Dao

--- app/src/main/java/com/example/bargraph/data/EmployeeDao.kt
+++ app/src/main/java/com/example/bargraph/data/EmployeeDao.kt
@@ -1,4 +1,15 @@
 package com.example.bargraph.data
 
-class EmployeeDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface EmployeeDao{
+            @Insert(onConflict = OnConflictStrategy.REPLACE)
+            suspend fun saveAllEmployees(quotes: List<Employee>)
+
+            @Query("SELECT * FROM Employee")
+            fun getEmployees(): List<Employee>
 }
