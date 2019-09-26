0e7541eb2ecf61bbb0a2930ed10ddcdf4d5f3b03
chrisribia <chrisribia@gmail.com>
2019-09-26 14:15:09 +0300

    Add MapViewModelFactory

--- app/src/main/java/com/example/bargraph/ui/MapFragment/MapViewModel.kt
+++ app/src/main/java/com/example/bargraph/ui/MapFragment/MapViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.bargraph.ui.MapFragment
 
 import androidx.lifecycle.ViewModel
+import com.example.bargraph.data.Repository.EmployeeRepository
 
-class MapViewModel : ViewModel() {
+class MapViewModel(private val repository: EmployeeRepository) : ViewModel() {
 }
---
+++ app/src/main/java/com/example/bargraph/ui/MapFragment/MapViewModelFactory.kt
@@ -0,0 +1,14 @@
+package com.example.bargraph.ui.MapFragment
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.bargraph.GraphViewModel
+import com.example.bargraph.data.Repository.EmployeeRepository
+
+class MapViewModelFactory(private val repository: EmployeeRepository)
+    :  ViewModelProvider.NewInstanceFactory()
+{
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MapViewModel(repository) as T
+    }
+}
