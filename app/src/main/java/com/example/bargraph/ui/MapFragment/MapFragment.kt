aa92d3a8523ae221f0f32063046a4a5e727cb0f6
chrisribia <chrisribia@gmail.com>
2019-10-01 12:51:43 +0300

    final

--- app/src/main/java/com/example/bargraph/ui/MapFragment/MapFragment.kt
+++ app/src/main/java/com/example/bargraph/ui/MapFragment/MapFragment.kt
@@ -10,20 +10,40 @@ import android.view.ViewGroup
 import com.example.bargraph.R
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import kotlinx.android.synthetic.main.map_fragment.*
 import kotlinx.coroutines.Job
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.kodein
 import org.kodein.di.generic.instance
 
-class MapFragment : Fragment(), KodeinAware {
+class MapFragment : Fragment(), KodeinAware, OnMapReadyCallback {
+
 
     override val kodein by kodein()
     private val factory: MapViewModelFactory by instance()
+
     private lateinit var googleMap : GoogleMap
-    private lateinit var mapView : MapView
-    private lateinit var job: Job
+
 
     private lateinit var viewModel: MapViewModel
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        mapView.getMapAsync(this)
+
+
+        viewModel = ViewModelProviders.of(this, factory).get(MapViewModel::class.java)
+
+    }
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let {
+            googleMap = it
+        }
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,12 +52,6 @@ class MapFragment : Fragment(), KodeinAware {
         return inflater.inflate(R.layout.map_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
 
-
-        viewModel = ViewModelProviders.of(this, factory).get(MapViewModel::class.java)
-
-    }
 
 }
